31768b2f5c824a17c677bfb6d0cd75cd74b440f7
Kelechiodom10 <kelechiodom10@gmail.com>
2022-01-19 01:47:31 +0100

    Set up swiper js to work with Next js and added custom styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { theme } from "@/utils/theme";
 import Layout from "@/components/layout/Layout";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../styles/swiper.css";
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
