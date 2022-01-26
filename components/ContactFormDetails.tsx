c942461d8af0d90646cc36223b2e807c34031930
Kelechiodom10 <kelechiodom10@gmail.com>
2022-01-26 17:33:28 +0000

    Updated form to prepopulate subject field when navigating from individual property page

--- components/ContactFormDetails.tsx
+++ components/ContactFormDetails.tsx
@@ -8,9 +8,36 @@ import {
   Stack,
   Textarea,
 } from "@chakra-ui/react";
-import React from "react";
+import { useRouter } from "next/router";
+import { parse } from "querystring";
+import React, { useEffect, useState } from "react";
 
 export default function ContactFormDetails() {
+  const router = useRouter();
+  const { asPath } = router;
+
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const { firstName, lastName, email, subject, message } = values;
+
+  useEffect(() => {
+    const query = parse(asPath.substr(asPath.indexOf("?") + 1));
+    const subject: string = query.subject as string;
+    if (subject) {
+      setValues({ ...values, subject });
+    }
+  }, [asPath]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
   return (
     <Box my={2} mx={{ base: 0, md: 4 }} textAlign="left">
       <form>
@@ -27,6 +54,9 @@ export default function ContactFormDetails() {
                 First Name
               </FormLabel>
               <Input
+                name="firstName"
+                value={firstName}
+                onChange={handleChange}
                 type="text"
                 placeholder="Enter first name"
                 fontSize={{ base: "sm", md: "md" }}
@@ -37,6 +67,9 @@ export default function ContactFormDetails() {
                 Last Name
               </FormLabel>
               <Input
+                name="lastName"
+                value={lastName}
+                onChange={handleChange}
                 type="text"
                 placeholder="Enter last name"
                 fontSize={{ base: "sm", md: "md" }}
@@ -48,6 +81,9 @@ export default function ContactFormDetails() {
               Email address
             </FormLabel>
             <Input
+              name="email"
+              value={email}
+              onChange={handleChange}
               type="email"
               placeholder="Enter your email address"
               fontSize={{ base: "sm", md: "md" }}
@@ -58,6 +94,9 @@ export default function ContactFormDetails() {
               Subject
             </FormLabel>
             <Input
+              name="subject"
+              value={subject}
+              onChange={handleChange}
               type="text"
               placeholder="Enter reason for contact"
               fontSize={{ base: "sm", md: "md" }}
@@ -68,6 +107,10 @@ export default function ContactFormDetails() {
               Message
             </FormLabel>
             <Textarea
+              name="message"
+              value={message}
+              onChange={handleChange}
+              fontSize={{ base: "sm", md: "md" }}
               resize="none"
               height="100px"
               variant="filled"
