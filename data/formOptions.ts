97d9524134ddf1ed04d29fa261c5dddce846d485
Kelechiodom10 <kelechiodom10@gmail.com>
2022-02-01 19:19:24 +0000

    Created file to hold all search form options

---
+++ data/formOptions.ts
@@ -0,0 +1,57 @@
+//Bedroom input values dropdown
+export const bedrooms = [
+  { value: 1, label: "1+" },
+  { value: 2, label: "2+" },
+  { value: 3, label: "3+" },
+  { value: 4, label: "4+" },
+  { value: 5, label: "5+" },
+  { value: 6, label: "6+" },
+  { value: 7, label: "7+" },
+  { value: 8, label: "8+" },
+  { value: 9, label: "9+" },
+  { value: 10, label: "10+" },
+];
+
+//Bathroom input values dropdown
+export const bathrooms = [
+  { value: 1, label: "1+" },
+  { value: 2, label: "2+" },
+  { value: 3, label: "3+" },
+  { value: 4, label: "4+" },
+  { value: 5, label: "5+" },
+  { value: 6, label: "6+" },
+  { value: 7, label: "7+" },
+  { value: 8, label: "8+" },
+  { value: 9, label: "9+" },
+  { value: 10, label: "10+" },
+];
+
+//Property types dropdown menu items
+export const propertyTypes = [
+  { value: "", label: "All" },
+  { value: "apartment", label: "Apartment" },
+  { value: "house", label: "House" },
+  { value: "flat", label: "Flat" },
+  { value: "studio", label: "Studio" },
+];
+
+//Minimum pricing options
+export const minPriceOptions = [
+  { value: "None", label: "None" },
+  { value: 200000, label: "£200,000" },
+  { value: 250000, label: "£250,000" },
+  { value: 300000, label: "£300,000" },
+];
+
+//Maximum pricing options
+export const maxPriceOptions = [
+  { value: "Max", label: "Max" },
+  { value: 7000000, label: "£7,000,000" },
+];
+
+//Furnishing options: conditionally on let form
+export const furnishing = [
+  { value: "Any", label: "Any" },
+  { value: "Furnished", label: "Furnished" },
+  { value: "Unfurnished", label: "Unfurnished" },
+];
