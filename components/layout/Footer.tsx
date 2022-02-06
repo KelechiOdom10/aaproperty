6ed65377c5ee80789010c6063d0ab50c49d72ae1
Kelechiodom10 <kelechiodom10@gmail.com>
2022-02-06 17:50:24 +0000

    Updated footer to reflect newly updated social links

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,10 +9,9 @@ import {
   Image,
   IconButton,
   Icon,
-  Button,
   useDisclosure,
 } from "@chakra-ui/react";
-import { FaInstagram, FaTwitter, FaPinterest } from "react-icons/fa";
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 import { FOOTER_ITEMS } from "@/data/footerItems";
 
 const Logo = () => {
@@ -42,6 +41,8 @@ const SocialButton = ({
       variant="ghost"
       as="a"
       href={href}
+      target="_blank"
+      rel="noopener noreferrer"
       rounded="full"
       icon={<Icon as={icon} w={8} h={8} />}
     />
@@ -60,32 +61,45 @@ export default function Footer() {
 
   return (
     <Box bg="gray.50" color="gray.700" as="footer">
-      <Container as={Stack} maxW={"6xl"} py={10}>
+      <Container as={Stack} maxW="6xl" py={10}>
         <SimpleGrid
           templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 1fr 1fr" }}
           spacing={8}
         >
           <Stack spacing={6}>
             <Logo />
-            <Text fontSize={"sm"}>
+            <Text fontSize="sm">
               © {new Date().getFullYear()} AA Property Ltd. All rights reserved
             </Text>
-            <Stack direction={"row"} spacing={6}>
-              <SocialButton label={"Twitter"} href={"#"} icon={FaTwitter} />
-              <SocialButton label={"Pinterest"} href={"#"} icon={FaPinterest} />
-              <SocialButton label={"Instagram"} href={"#"} icon={FaInstagram} />
+            <Stack direction="row" spacing={6}>
+              <SocialButton
+                label="Twitter"
+                href="https://twitter.com/aapropertyukltd"
+                icon={FaTwitter}
+              />
+              <SocialButton
+                label="Instagram"
+                href="https://www.instagram.com/aapropertyuk"
+                icon={FaInstagram}
+              />
+              <SocialButton
+                label="Facebook"
+                href="https://www.facebook.com/aapropertyuk/?sk=allactivity&privacy_source=activity_log&log_filter=cluster_114"
+                icon={FaFacebook}
+              />
             </Stack>
           </Stack>
 
           {FOOTER_ITEMS.map((item) => {
             return (
-              <Stack align={"flex-start"} key={item.header}>
+              <Stack align="flex-start" key={item.header}>
                 <ListHeader>{item.header}</ListHeader>
                 {item.children.map((linkItem) => (
                   <Link
                     key={linkItem.label}
                     fontSize={{ base: "sm", md: "md" }}
                     href={linkItem.href}
+                    isExternal={item.header === "Follow Us"}
                   >
                     {linkItem.label}
                   </Link>
